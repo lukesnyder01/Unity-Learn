76647105c16e40ac61f6bd1eb6679b40e6c55db3
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2024-12-22 08:11:20 -0800

    Add random spawn timer

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -8,21 +8,41 @@ public class SpawnManager : MonoBehaviour
     private float spawnRangeX = 20;
     private float spawnPosZ = 35;
 
+    private float timer;
+    private float minSpawnTime = 0.4f;
+    private float maxSpawnTime = 0.6f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetTimer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            var index = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        timer -= Time.deltaTime;
 
-            Instantiate(animalPrefabs[index], spawnPos, animalPrefabs[index].transform.rotation);
+        if (timer <= 0f)
+        {
+            SetTimer();
+            SpawnRandomAnimal();
         }
     }
+
+    void SetTimer()
+    {
+        timer = Random.Range(minSpawnTime, maxSpawnTime);
+    }
+
+
+    void SpawnRandomAnimal()
+    {
+        var index = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[index], spawnPos, animalPrefabs[index].transform.rotation);
+
+    }
+
 }
