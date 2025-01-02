aef38b0dd18a36587564f1345bb93aafd80a7ff4
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2025-01-01 20:07:20 -0800

    Fix challenge bugs

--- Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,27 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private float timer;
+    public float timeBetweenShots = 2f;
+
+    void Start()
+    {
+        timer = timeBetweenShots;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        timer -= Time.deltaTime;
+
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            if (timer <= 0)
+            {
+                timer = timeBetweenShots;
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
         }
     }
 }
