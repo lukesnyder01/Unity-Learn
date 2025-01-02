b58d314e6e51ba75ff18417f2d462784ef25f1b1
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2025-01-02 09:03:31 -0800

    Add bouncing effect

--- Mod the Cube/Assets/ModTheCube/Cube.cs
+++ Mod the Cube/Assets/ModTheCube/Cube.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    
+
+    float rotationSpeed = 30f;
+
+
+    float bounceSpeed = 8f;
+    float bounceScale = 8f;
+
+    private Vector3 defaultScale = new Vector3(3f, 3f, 3f);
+
     void Start()
     {
         transform.position = new Vector3(3, 4, 1);
@@ -18,6 +26,21 @@ public class Cube : MonoBehaviour
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        RotateCube();
+        ChangeCubeScale();
     }
+
+
+    void RotateCube()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    void ChangeCubeScale()
+    {
+        var targetScaleMultiplier = (Mathf.Sin(Time.time * bounceSpeed) + 8) / bounceScale;
+        transform.localScale = defaultScale * targetScaleMultiplier;
+    }
+
+
 }
