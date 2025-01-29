0885a5aa379a42b8cae580864cf25a330eb48676
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2025-01-29 09:33:28 -0800

    feat(target): add random spawn parameters

--- Prototype 5/Assets/Scripts/Target.cs
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody rb;
+
+    private float minForce = 12;
+    private float maxForce = 15;
+
+    private float minTorque = -10;
+    private float maxTorque = 10;
+
+    private float xSpawnRange = 4;
+    private float ySpawnPos = -6;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(RandomForce(), ForceMode.Impulse);
+        rb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPosition();
     }
 
-    // Update is called once per frame
-    void Update()
+    Vector3 RandomForce()
     {
-        
+        return Vector3.up * Random.Range(minForce, maxForce);
     }
+
+    float RandomTorque()
+    {
+        return Random.Range(minTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPosition()
+    { 
+        return new Vector3(Random.Range(-xSpawnRange, xSpawnRange), ySpawnPos, 0);
+    }
+
 }
